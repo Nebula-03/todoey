eba6004ea0d945cc15d33dcefa08e1a933d0eb6a
Nikita Mandal <nikki.space03@gmail.com>
2026-01-04 17:20:04 +0530

    second commit

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/widgets/tasks_list.dart';
+
+class TasksScreens extends StatelessWidget {
+  const TasksScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan,
+      floatingActionButton: FloatingActionButton(
+        elevation: 15,
+        backgroundColor: Colors.cyan,
+        onPressed: () {  },
+        shape: CircleBorder(),
+        child: Icon(Icons.add, color: Colors.white,),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list, color: Colors.cyan,size: 40,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(height: 10,),
+                Text('Todoey', style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 40)),
+                Text(
+                  '12 Tasks', style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.w400),
+                ),
+
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45),
+                ),
+              ),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
